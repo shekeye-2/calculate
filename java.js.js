4685be351155612a62b6b306ecf1c8bc24b96931
shekeye-2 <shekeeye1113@gmail.com>
2026-05-02 10:06:38 -0700

    Add files via upload

---
+++ java.js.js
@@ -0,0 +1,12 @@
+// calculater progarm
+const display = document.getElementById("display");
+
+function appendtodisplay(input){
+  display.value += input;
+}
+function cleardisplay(){
+  display.value ="";
+}
+function calculate(){
+ display.value = eval(display.value);
+}
